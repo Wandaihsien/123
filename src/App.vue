19497fd3b0b3266a290e64daf593f84afa5a1653
wan <graywolf7235@gmail.com>
2024-11-21 18:08:54 +0800

    '123'

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <!-- <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <nav>
+    <RouterLink to="/card-info">Card Info</RouterLink>
+    <RouterLink to="/work-shop">Work Shop</RouterLink>
+    <RouterLink to="/hero-member">Hero Member</RouterLink>
+    <RouterLink to="/social-article">Social Article</RouterLink>
+  </nav> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+  padding: 0 4px;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CardInfo from "../views/CardInfo.vue";
+import WorkShopView from "@/views/WorkShopView.vue";
+import HeroMemberView from "@/views/HeroMemberView.vue";
+import SocialArticle from "@/views/SocialArticle.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/card-info",
+      name: "card-info",
+      component: CardInfo,
+    },
+    {
+      path: "/work-shop",
+      name: "work-shop",
+      component: WorkShopView,
+    },
+    {
+      path: "/hero-member",
+      name: "hero-member",
+      component: HeroMemberView,
+    },
+    {
+      path: "/social-article",
+      name: "social-article",
+      component: SocialArticle,
+    }
+  ],
+});
+
+export default router;
